d55fd236b83f9d3d7dbe0abd78d0e441c571dfa1
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-16 21:38:30 +0100

    done with the sidebar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const view1 = document.getElementById('navView1');
-  const view2 = document.getElementById('navView2');
+  const view1 = document.getElementById('sidebar-display1');
+  const view2 = document.getElementById('sidebar-display2');
 
   const toggleBtns = document.querySelectorAll('.toggleNavView');
 
